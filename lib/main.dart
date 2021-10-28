23d4c7fc059059dc143763cb700bb36c37724e74
Aditya <60613717+Aditya7feb@users.noreply.github.com>
2021-10-28 15:43:20 +0530

    assets

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:sample_app/login.dart';
 
 void main() {
   runApp(MyApp());
@@ -8,12 +10,13 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return GetMaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Vendor Version',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
+      home: Login(),
     );
   }
 }
